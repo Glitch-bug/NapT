1c73c739748ed9abef998ec77db07089cf19f573
kwabena yiadom <kwabenayiadom271@gmail.com>
2022-05-05 13:21:47 +0000

    slider animation

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ backheight = backheight.slice(0,-2)
 console.log(backheight)
 document.addEventListener("DOMContentLoaded", function() {
     
-    let fps = setInterval(raise, 1000/60);
+    const fps = setInterval(raise, 1000/60);
 });
 
 var raise = function() {
     backheight = backheight - 1.5;
     var num = backheight;
     document.querySelector(".background").style.height = `${num}px`; 
+    if (backheight <= 1){
+        document.querySelector(".background").style.display = 'none';
+        clearInterval(fps);
+    }
 };
