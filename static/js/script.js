48330c195bc43fbb70ec97e48e6d31e447adcd7a
kwabena yiadom <kwabenayiadom271@gmail.com>
2022-07-04 20:41:29 +0000

    homepage 3.0

--- static/js/script.js
+++ static/js/script.js
@@ -1,36 +1,73 @@
-let reveal = () => {
+var reveal = () => {
   let section = document.getElementsByTagName("section");
-  let wrp = document.querySelectorAll(".sect-wrapper");
 
   let nalink = document.querySelectorAll(".nav-item");
   let naan = document.querySelectorAll(".nav-item > a");
 
   for(let i = 0; i < section.length; i++){
     let windowHeight = window.innerHeight;
-    let sectop = section[i].getBoundingClientRect().top;
+    let sectop = document.getElementsByTagName("section")[i].getBoundingClientRect().top;
     let revealpoint = 300;
-  
-    if(sectop < windowHeight - revealpoint){
-        wrp[i].classList.add("active");
+    let show = false;
+    let wrp = document.querySelector(`#${section[i].id} .sect-wrapper`);
+    //You made a mistake booooo!!! 
+    // Obtain id of sectop then make active sect-wrapper within that sectop
+    if (wrp){
+      if((sectop < windowHeight - revealpoint)){
+        wrp.classList.add("active");
+      }else{
+        wrp.classList.remove("active");
       }
-        else{
-        wrp[i].classList.remove("active");
-      }
+    }
   }
 
-//   for(let i = 0; i < nalink.length; i++){
-//     let windowHeight = window.innerHeight;
-//     let sectop = section[i].getBoundingClientRect().top;
+  for(let i = 0; i < nalink.length; i++){
+    let windowHeight = window.innerHeight;
+    let sectop = section[i].getBoundingClientRect().top;
 
-//     if(sectop < windowHeight - 100){
-//         nalink[i].classList.add("scroll");
-//     }
-//   }
+    if(sectop < windowHeight - 100){
+        nalink[i].classList.add("scroll");
+    }
+  }
+  console.log("Just keep scrolling");
 }
 
-// ping pong window
-// fps = setInterval();
 
 var divWindow = document.querySelector("#glass-ping");
-divWindow.style.top = -300;
-window.addEventListener('scroll', reveal);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  window.addEventListener('scroll', reveal);
+  const fps = setInterval(ping_pong, 1000/60);
+});
+var x_ch = 1;
+var y_ch = 1;
+
+var background_height = window.getComputedStyle(document.querySelector('.home-backk')).height.slice(0,-2);
+var background_width = window.getComputedStyle(document.querySelector('.home-backk')).width.slice(0,-2);
+var ping_pong = function() {
+
+  //Glass vertical motion animation
+  var glass_height = 300;
+  var glass_width = 500;
+  let t_num = parseInt(window.getComputedStyle(document.querySelector('#glass-ping')).top.slice(0,-2));
+  if(t_num <= 0){
+    y_ch = 1;
+  }
+  else if(t_num >= parseInt(background_height)-glass_height){
+    y_ch = -1;
+  }
+  t_num += y_ch;
+  document.querySelector('#glass-ping').style.top = t_num+"px";
+  
+  //Glass horizontal motion animation
+  let l_num = parseInt(window.getComputedStyle(document.querySelector('#glass-ping')).left.slice(0,-2));
+  if(l_num <= 0){
+    x_ch = 1;
+  }
+  else if(l_num >= parseInt(background_width)-glass_width){
+    x_ch = -1;
+  }
+  l_num += x_ch;
+  document.querySelector('#glass-ping').style.left = l_num+"px";
+};
